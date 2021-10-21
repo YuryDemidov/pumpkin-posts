8965eb2c4fd02bc62df30857b12036f68e2857d2
Yury Demidov <demidov_uriy@poisondrop.ru>
2021-10-22 01:39:41 +0300

    Add posts list animation

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import PostItem from './PostItem'
 
 const PostsList = ({ posts, removePost }) => {
-  return (
-    <>
-      {posts.length ? (
-        posts.map((post, i) => <PostItem post={post} number={i + 1} removePost={removePost} key={post.id} />)
-      ) : (
-        <p>Посты не найдены</p>
-      )}
-    </>
+  return posts.length ? (
+    <TransitionGroup>
+      {posts.map((post, i) => (
+        <CSSTransition
+          key={post.id}
+          timeout={{
+            enter: 1000,
+            exit: 400
+          }}
+          classNames='post'
+        >
+          <PostItem post={post} number={i + 1} removePost={removePost} />
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
+  ) : (
+    <p>Посты не найдены</p>
   )
 }
 
